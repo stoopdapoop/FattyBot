7ae4ed3b3d4ef8cb8c188695f6ed72e7dab4ed4d
stoopdapoop <dreamlandkirby@gmail.com>
2014-09-25 22:15:54 -0400

    removed unnecessary usings

--- WolframAPI.cs
+++ WolframAPI.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Xml;
-using System.Threading.Tasks;
-using System.Web;
-using Newtonsoft.Json;
 using System.Net;
 using System.IO;
 
